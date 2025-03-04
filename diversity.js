0348bcb537154717500b9156237cfa15ffc2271d
rasithm <mohamedrasith@gmail.com>
2025-03-04 17:09:57 +0530

    completed

--- diversity.js
+++ diversity.js
@@ -2,8 +2,10 @@ const openBox = document.getElementById('open-fa-caret-right');
 const employementBody = document.getElementById('employement-body');
 
 openBox.addEventListener('click', () => {
+    const rRight = document.getElementById('r-right')
     if (employementBody.style.display === 'none' || employementBody.style.display === '') {
-        employementBody.style.display = 'flex'; 
+        employementBody.style.display = 'flex';
+        rRight.style.height = '1300px' 
     } else {
         employementBody.style.display = 'none';
     }
@@ -15,10 +17,13 @@ const openBoxEducation = document.getElementById('open-education');
 const educationBody = document.getElementById('education-body');
 
 openBoxEducation.addEventListener('click', () => {
+    const rRight = document.getElementById('r-right')
     if (educationBody.style.display === 'none' || educationBody.style.display === '') {
         educationBody.style.display = 'flex'; 
+        rRight.style.height = '1500px' 
     } else {
         educationBody.style.display = 'none';
+        
     }
 });
 
@@ -30,10 +35,13 @@ const openBoxDiversity = document.getElementById('open-diversity');
 const diversityBody = document.getElementById('diversity-body');
 
 openBoxDiversity.addEventListener('click', () => {
+    const rRight = document.getElementById('r-right')
     if (diversityBody.style.display === 'none' || diversityBody.style.display === '') {
+        rRight.style.height = '1800px'
         diversityBody.style.display = 'flex'; 
     } else {
         diversityBody.style.display = 'none';
+        rRight.style.height = '1500px'
     }
 });
 
